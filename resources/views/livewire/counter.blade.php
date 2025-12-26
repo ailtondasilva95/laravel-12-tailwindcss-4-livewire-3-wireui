da5aed395682ba3e9ae4ba480597bf19e70e4107
ailtondasilva95 <ailton.domingos.dias.da.silva@gmail.com>
2025-12-26 09:22:33 +0100

    Implementação do livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div class="flex flex-col items-center justify-center min-h-screen gap-4">
+    <h1 class="text-3xl font-bold underline">{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-layouts.app>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <p class="text-center">
+        Welcome to Laravel 12 with Tailwind CSS 4, Livewire 3, Alpine.js, WireUI, and PowerGrid!
+    </p>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    @livewire('counter')
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-    <!-- Styles -->
-    @vite(['resources/css/app.css'])
-</head>
-
-<body>
-
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-
-    <!-- Scripts -->
-    @vite(['resources/js/app.js'])
-</body>
-
-</html>
+</x-layouts.app>
